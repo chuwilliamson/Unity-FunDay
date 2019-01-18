1232073b40a5cc2d879bb832ee2462d40367e325
Jordon Cotton <s188044@students.aie.edu>
2019-01-18 13:22:37 -0600

    Do it - Mr. Matt

--- Assets/Scripts/SpawnerBehaviour.cs
+++ Assets/Scripts/SpawnerBehaviour.cs
@@ -2,23 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnerBehaviour : MonoBehaviour {
+public class SpawnerBehaviour : MonoBehaviour
+{
     public GameObject prefab;
     public List<GameObject> objects;
-    int num = 5;
+    public int count;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         Debug.Log("Begin Spawn");
-        for(int i = 0; i < num; i++)
+       
+        for(int i = 0; i < count; i++)
         {
             GameObject go = Instantiate(prefab);
             objects.Add(go);
         }
-        
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        
 	}
 }
