db4e96130ae241450754a6f1442e430c43890c45
Matthew Williamson <matthew.williamson@aie.edu.au>
2019-01-18 13:08:55 -0600

    init commit

---
+++ Assets/Scripts/SpawnerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerBehaviour : MonoBehaviour {
+    public GameObject prefab;
+    public List<GameObject> objects;
+    int num = 5;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Begin Spawn");
+        for(int i = 0; i < num; i++)
+        {
+            GameObject go = Instantiate(prefab);
+            objects.Add(go);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
